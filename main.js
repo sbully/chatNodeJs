2d75ee0d682b3a0f5d9287f9e91f259baec6b8c7
sbully <43850116+sbully@users.noreply.github.com>
2020-04-09 17:03:10 +0200

    update broadcast

--- main.js
+++ main.js
@@ -13,25 +13,32 @@ app.get('/', function (requete, reponse) {
 	reponse.setHeader("Content-Type", "text/html");
 	reponse.status(200);
 	reponse.render('logpage.ejs');
-})
+});
 app.get('/home', function (requete, reponse) {
 	reponse.redirect('/');
-})
+});
 
 app.get('/chat', function (requete, reponse) {
 	reponse.setHeader("Content-Type", "text/html");
 	reponse.status(200);
 	reponse.render('chatpage.ejs');
-})
+});
 
 app.use(function (requete, reponse) {
 	reponse.setHeader('Content-Type', 'text/plain');
 	reponse.status(404).send("Erreur 404 : Page introuvable");
-})
+});
 
 io.sockets.on('connection', function (socket) {
-	console.log('un client est connecté');
-	socket.emit('login', 'Bienvenue, vous êtes connecté!')
-})
+	console.log('New Client');
+	socket.broadcast.emit('message', "nouveau utilisateur connecté")
+	socket.emit('login', 'Bienvenue, vous êtes connecté!');
+	socket.on('message', function (message) {
+		console.log("message reçu :" + message)
+	});
+
+});
+
+
 
 /* app.listen(8080); */
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,22 @@
-var socket = io.connect('http://localhost:8080');
+let socket = io.connect('http://localhost:8080');
+let sendbut = document.querySelector('.submit');
+let messageText = document.querySelector('.Message');
+let chat = document.querySelector('#chatsection');
+
+console.log(chat);
 
 
 socket.on('login', function (message) {
-	alert("Message du serveur :\n" + message);
-})
+	console.log(message);
+});
+
+socket.on('message', function (message) {
+	console.log(message);
+});
+
+sendbut.addEventListener('click', (event) => {
+	console.log(messageText.value);
+	socket.emit('message', messageText.value);
+	event.preventDefault();
+});
+
